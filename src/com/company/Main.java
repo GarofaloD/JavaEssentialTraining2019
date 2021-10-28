5e8454024231a4673ed77f74ed179a350299a553
garofalod <garofalodaniel@gmail.com>
2021-10-28 14:43:08 -0500

    This is a commit test in a forked branch from another project (with a comment and push)

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -7,5 +7,20 @@ public class Main {
         String answer = "The answer is " + result;
         //this comment goes as well
         System.out.println(answer);
+
+        String howMany = 20 + " Things";
+        System.out.println(howMany);
+
+        int intValue = 42;
+        var fromInt = Integer.toString(intValue);
+        System.out.println(fromInt);
+
+        boolean boolValue = true;
+        var fromBool = Boolean.toString(boolValue);
+        System.out.println(fromBool);
+
+        long longValue = 10_000_000;
+        var fromLong = Long.toString(longValue);
+        System.out.println(fromLong);
     }
 }
