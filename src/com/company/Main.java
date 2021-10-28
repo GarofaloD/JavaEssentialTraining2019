a4d5109c6119cdf6337ff13b4c0a6f0bba517500
garofalod <garofalodaniel@gmail.com>
2021-10-28 14:35:08 -0500

    This is a commit test in a forked branch from another project (with a comment and push)

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,6 +5,7 @@ public class Main {
     public static void main(String[] args) {
         int result = 10 * 12;
         String answer = "The answer is " + result;
+        //this comment goes as well
         System.out.println(answer);
     }
 }
