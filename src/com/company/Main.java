c86fadb04d1d71f176c8e835c1d8929adeeb62d8
garofalod <garofalodaniel@gmail.com>
2021-11-08 09:38:45 -0600

    String interpolation, with some added cool stuff

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,13 +1,20 @@
 package com.company;
 
 public class Main {
-
+ //String interpolation
     public static void main(String[] args) {
 
         var item = "Shirt";
         var size = "M";
         var price = 14.99;
         var color = "Red";
+
+        var template = "Clothing item: %s, size %s, color %s, $%.2f";
+        //This is a way that we can do template literals and formatting at the same time
+
+        var itemString = String.format(template, item, size, color, price);
+        System.out.println(itemString);
+
 
     }
 }
