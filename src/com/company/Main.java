04e64902ff7d5fded206cb25550ef48664f03ba8
danielgarofalo <garofalodaniel@gmail.com>
2021-11-11 16:37:41 -0600

    Basic if - LocalDateTime

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,14 +1,37 @@
 package com.company;
 
+import java.time.LocalDateTime;
 import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
 
-        var scanner = new Scanner(System.in);
-        System.out.print("Enter a month number (1-12): ");
-        var monthNumber = scanner.nextInt();
+//        var scanner = new Scanner(System.in);
+//        System.out.print("Enter a month number (1-12): ");
+//        var monthNumber = scanner.nextInt();
 
-    }
-}
+        var now = LocalDateTime.now(); //Returns current moment in time
+        var monthNumber = now.getMonthValue();
+
+        //Basic if
+        String message;
+//        if (monthNumber < 1 || monthNumber > 12 ){
+//            message = "This is not a valid number";
+//        } else
+
+         if (monthNumber <= 3){
+            message = "That is in Q1";
+        } else if (monthNumber <= 6 ){
+            message = "That is in Q2";
+        } else if (monthNumber <= 9){
+            message = "That is in Q3";
+        } else {
+            message = "That is in Q4";
+        }
+
+        System.out.println(message);
+
+
+    }//:PSVM
+}//:MAIN
