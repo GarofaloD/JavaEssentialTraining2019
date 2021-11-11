41e96e157eb962c4bf27beeb9f0f9fd816e7b283
danielgarofalo <garofalodaniel@gmail.com>
2021-11-11 12:53:54 -0600

    Scanner challenge

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,21 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Please, enter the first value:");
+        var num1 = scanner.nextDouble();
+        System.out.println("Please, enter the second value:");
+        var num2 = scanner.nextDouble();
+
+        double result = num1 / num2;
+
+        System.out.println("The result is: " + result);
     }
 }
