ae6152d83d74efc7b14619de9c5759c1bdce535f
danielgarofalo <garofalodaniel@gmail.com>
2021-11-11 17:02:44 -0600

    Basic looping - for, enhanced for, streams(??) , while , do while

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,5 +1,7 @@
 package com.company;
 
+import java.util.Arrays;
+
 public class Main {
 
     public static void main(String[] args) {
@@ -9,6 +11,33 @@ public class Main {
                         "April", "May", "June",
                         "July", "August", "September",
                         "October", "November", "December"};
+
+
+        //Basic ForLoop - Old way
+        for (int i = 0; i < months.length; i++) {
+            System.out.println(months[i] + "Old way");
+        }
+
+        //Basic for loop - New way
+        for (String month : months) { //creates a map out of the elements of the array into a string
+            System.out.println(month + "New way");
+        }
+
+        //Stream??
+        Arrays.stream(months).forEach(System.out::println);
+
+        //While loop basic
+        var whileCounter = 0;
+        while (whileCounter < months.length){
+            System.out.println(months[whileCounter++] + " with While loop");
+        }
+
+        //do while loop basic
+        var doWhileCounter = 0;
+        do{
+            System.out.println(months[doWhileCounter] + " with do while basic");
+            doWhileCounter++;
+        } while(doWhileCounter < months.length);
 
     }
 }
