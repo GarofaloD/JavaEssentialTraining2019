588c8552d7d992a0c5a8b44fe870c6674d4f7995
danielgarofalo <garofalodaniel@gmail.com>
2021-11-11 16:49:42 -0600

    Switch (the new version)

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -9,5 +9,16 @@ public class Main {
         var now = LocalDateTime.now();
         var monthNumber = now.getMonthValue();
 
+        //Switch statements : this is the new one
+        switch (monthNumber) {
+            case 1 -> System.out.println("Month is January");
+            case 2 -> System.out.println("Month is Feb");
+            case 3 -> System.out.println("Month is March");
+            case 4 -> System.out.println("Month in April");
+            case 5 -> System.out.println("Month is May");
+            case 6,7,8,9 -> System.out.println("This is mid year");
+            default -> System.out.println("this is by the end of the year");
+        }
+
     }
 }
