32fabfb972432eb0b24b8788271c571e3fc4a25b
danielgarofalo <garofalodaniel@gmail.com>
2021-11-11 17:13:01 -0600

    Method refactoring - basic

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -8,16 +8,22 @@ public class Main {
 
         var sc = new Scanner(System.in);
 
-        System.out.print("Enter a numeric value: ");
-        var d1 = sc.nextDouble();
-
-        System.out.print("Enter a numeric value: ");
-        var d2 = sc.nextDouble();
+        //Method invocation - basic
+        double d1 = getInput(sc, "Enter the first value:");
+        double d2 = getInput(sc, "Enter the second value:");
 
         double result = d1 / d2;
 
         System.out.println("The answer is " + result);
+
+
+    }//:PSVM
+
+    //Basic method refactor - passing values
+    private static double getInput(Scanner sc, String prompt) {
+        System.out.print(prompt);
+        return sc.nextDouble();
     }
 
-}
+}//:MAIN
 
