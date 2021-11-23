d4ff8ce1df97cf325968b97fce83ef33a2e3b79d
garofalod <garofalodaniel@gmail.com>
2021-11-23 16:35:12 -0600

    Multi catch

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,12 +4,25 @@ public class Main {
 
     public static void main(String[] args) {
 
-        String s = null;
+        //multiple catch blocks
+        String s = "";
+
         try {
             var sub = s.substring(1);
-        } catch (Exception e) {
+        } catch (NullPointerException e) { //NullPointerException is a class that manages null point errors
+            //If we limit the exception handlers and we get a different type of error, the catch might not work and
+            //the program can break
             e.printStackTrace();
+            System.out.println("Null pointer: " + e.getMessage());//getting the message from the exception manager
+            //In this case, the NullPointerException
+        } catch (StringIndexOutOfBoundsException e){
+            e.printStackTrace();
+            System.out.println("Out of bounds: " + e.getMessage());//
+        } catch (Exception e){ //Exception will catch all exceptions, no matter the origin
+            e.printStackTrace();
+            System.out.println("Any exception: " + e.getMessage());//getting the message from the exception manager
         }
+
         System.out.println("I'm not dead yet!");
     }
 }
