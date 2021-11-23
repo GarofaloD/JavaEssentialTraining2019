85655951555f9549c6a4777d7988809761b0eefc
garofalod <garofalodaniel@gmail.com>
2021-11-23 15:39:25 -0600

    try/catch basic

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,22 @@
+
 package com.company;
 
 public class Main {
 
+    //Handle exceptions with try/catch
     public static void main(String[] args) {
-	// write your code here
+
+        String s = null;
+
+        //Code > Surround with... > try/catch
+        try {
+            var sub = s.substring(1);
+        } catch (Exception e) { //The catch allows you to keep running without the breaking caused by the exception
+            e.printStackTrace();
+        }
+
+        System.out.println("I'm not dead yet");
     }
 }
+
+
