392740cd2e4103f358c55995de048f62443e21cf
danielgarofalo <garofalodaniel@gmail.com>
2021-12-04 12:12:02 -0600

    Constants

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,20 +1,23 @@
 package com.company;
-
 import com.company.model.ClothingItem;
-
 import java.text.NumberFormat;
 
 public class Main {
 
     public static void main(String[] args) {
 
-        var item = new ClothingItem("Shirt", "M", 19.99, 3);
+        //Use of the constant in the class in such way that we do not need to type and make mistakes
+        //The same thing could be applied for the sizes
+        var item = new ClothingItem(ClothingItem.SHIRT, ClothingItem.SIZE_M, 19.99, 3);
 
         var totalPrice = item.getPrice() * item.getQuantity();
         var formatter = NumberFormat.getCurrencyInstance();
-        var output = String.format("Your %s order will cost %s",
-                item.getType(),
-                formatter.format(totalPrice));
+        var output = String.format("Your %s order (size %s) will cost %s",
+                item.getType(), //first %s
+                item.getSize(), //second %s
+                formatter.format(totalPrice)); //third %s
         System.out.println(output);
     }
+
+
 }
--- src/com/company/model/ClothingItem.java
+++ src/com/company/model/ClothingItem.java
@@ -1,6 +1,17 @@
 package com.company.model;
 
 public class ClothingItem {
+
+    //Declaration of constants
+    public static final String SHIRT = "Shirt";
+    public static final String PANTS = "Pants";
+    public static final String SHOES = "Shoes";
+
+    public static final String SIZE_S = "S";
+    public static final String SIZE_M = "M";
+    public static final String SIZE_L = "L";
+    public static final String SIZE_XL = "XL";
+
     private String type;
     private String size;
     private double price;
