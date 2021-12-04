383aef6528ec35082f6a96144320a7e5cb1336f7
danielgarofalo <garofalodaniel@gmail.com>
2021-12-04 11:50:44 -0600

    custom class, instance field and methods

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,29 @@
 package com.company;
 
+import com.company.model.ClothingItem;
+
+import javax.swing.text.NumberFormatter;
+import java.text.NumberFormat;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
+        var item = new ClothingItem();//new instance of the custom class
+        item.setType("Shirt");
+        item.setSize("M");
+        item.setPrice(19.99);
+        item.setQuantity(3);
+
+        var totalPrice = item.getPrice() * item.getQuantity();
+        var formatter = NumberFormat.getCurrencyInstance();
+        var output = String.format(
+                "Your %s order will cost %s",
+                item.getType(), //first %s
+                formatter.format(totalPrice)); //second %s
+
+        System.out.println(output);
     }
+
+
 }
